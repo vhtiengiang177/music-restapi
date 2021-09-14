041a761c59d6688fea4b12def6ee4487acba91ad
TienGiang <vhtiengiang177@gmail.com>
2021-09-14 09:52:24 +0700

    Add project files.

---
+++ music-restapi/Controllers/SongsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using music_restapi.Models;
+
+namespace music_restapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private static List<Song> songs = new List<Song>() {
+            new Song(){Id=0, Title="A", Language="English" },
+            new Song(){Id=1, Title="B", Language="English" },
+            new Song(){Id=2, Title="C", Language="English" },
+            new Song(){Id=3, Title="D", Language="English" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Song song)
+        {
+            songs.Add(song);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Song song)
+        {
+            songs[id] = song;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            songs.RemoveAt(id);
+        }
+    }
+}
---
+++ music-restapi/Data/APIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using music_restapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace music_restapi.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
